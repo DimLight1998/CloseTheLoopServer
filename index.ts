a7149d90be087af6c1611d7d1bd4c81444ce8852
Zhang Yang <dimlight1998@gmail.com>
2018-07-16 17:04:40 +0800

    Change port.

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import WebSocket from 'ws';
 
-const wss: WebSocket.Server = new WebSocket.Server({ port: 8080 });
+const wss: WebSocket.Server = new WebSocket.Server({ port: 9990 });
 
 wss.on('connection', ws => {
     ws.on('message', message => {
